03d65e41aac563aa1dafb71e8c364740466acb86
JCalvinL <lkc3215@gmail.com>
2015-04-25 11:41:10 -0700

    Updating forked repository

--- helloworld.py
+++ helloworld.py
@@ -14,3 +14,33 @@ if (__name__ == '__main__'):
     printhello = HelloWorld()
     printhello.print_msg()
 
+    class Member():
+    first_name = ""
+    last_name = ""
+    email = ""
+    phone = ""
+    grad_year = ""
+    def is_current_member():
+        pass
+
+    class Indexer():
+        def search_by_name(name):
+            pass
+        def search_by_grad_year(year):
+            pass
+        def search_by_email(email):
+            pass
+        def search_by_membership(status):
+            pass
+
+billy = Member()
+billy.first_name = "Billy"
+billy.last_name = "Bob Thorton"
+billy.email = "Billy@bobthorton.com"
+billy.phone = "8947546789"
+billy.grad_year = "2015"
+
+billy.str()
+
+print billy
+
