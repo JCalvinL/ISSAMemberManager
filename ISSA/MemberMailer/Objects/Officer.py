3061a4d30eb3feeda0971493779308db16e48501
JCalvinL <lkc3215@gmail.com>
2015-05-23 11:05:16 -0700

    Enhanced Officer Class
    Not sure if this is an improvement... I think it's an improvement over
    the last attempt I had at python.

--- ISSA/MemberMailer/Objects/Officer.py
+++ ISSA/MemberMailer/Objects/Officer.py
@@ -1,17 +1,19 @@
 class Officer():
-
+    
+    first_name = ""
+    last_name = ""
     officerType = ""
-    secretary = ""
-    absoc_representative = ""
-    historian = ""
-    webmaster = ""
+    #Officer positions are: secretary, absoc representative, historian, and webmaster
+    board_year = ""
+    org_email = ""
+    elected = ""
 
-    def is_current_member(self):
-        pass
     def is_current_officer(self):
         pass
 
-    def createOfficer(self, officerType):
-        self.officerType=officerType
-    def displayOfficer(self):
-        return self.officerType
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    def createOfficer(self):
+        if(self == current) and (self == elected):
+            return self.officerType
